eb024d9039d1702d5e314aaaf6ed1fffd4de68e9
Joe Wasson <jwasson+git@gmail.com>
2011-05-06 18:54:59 -0700

    WIP for data-driven pages.

---
+++ Ninject.Website/Controllers/MerchandiseController.cs
@@ -0,0 +1,23 @@
+﻿#region License
+//
+// Author: Nate Kohari <nate@enkari.com>
+// Copyright (c) 2007-2010, Enkari, Ltd.
+//
+// Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+// See the file LICENSE.txt for details.
+//
+#endregion
+#region Using Directives
+using System.Web.Mvc;
+#endregion
+
+namespace Ninject.Website.Controllers
+{
+    public class MerchandiseController : Controller
+    {
+        public ViewResult Show()
+        {
+            return View();
+        }
+    }
+}
